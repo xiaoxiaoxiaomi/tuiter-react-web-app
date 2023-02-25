f39227a4eb121d1ad24e9965a75057c1ac01ee78
rxjiang <jiang.rux@northeastern.edu>
2023-02-25 00:28:21 -0800

    a6 React.js sp22

---
+++ src/tuiter/home/post-item.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./index.css";
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { faEllipsisH, faComment, faRetweet, faHeart, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const PostItem = (
+    {
+      post = {}
+    }
+) => {
+  return (
+      <li className="list-group-item">
+        <div className="row">
+          <div className="col-2">
+            <img className="img-fluid rounded-circle"
+                 src={`/images/${post.avatar}`}/>
+          </div>
+          <div className="col-10 ps-0">
+            <div className="row">
+              <div className="col-11">
+                <span className="fw-bold">{post.author}</span>
+                <FontAwesomeIcon icon={faCheckCircle} />
+                <span
+                    className="text-dark">{post.handle} &middot; {post.time}</span>
+              </div>
+              <div className="col-1">
+
+                <FontAwesomeIcon icon={faEllipsisH} />
+              </div>
+            </div>
+            <div>{post.tweetText}</div>
+            <div className={`mt-2 border border-dark rounded-4 ${post.imageTitle
+                ? ''
+                : 'd-none'}`}>
+              <img src={`/images/${post.image}`}
+                   className="border-bottom border-dark img-fluid tweet-img"/>
+              <div className="p-2">
+                <div>{post.imageTitle}</div>
+                <div className="text-dark">{post.imageContent}</div>
+              </div>
+            </div>
+            <div className={`mt-2 border border-dark rounded-4 ${post.imageTitle
+                ? 'd-none' : ''}`}>
+              <img src={`/images/${post.image}`}
+                   className="img-fluid rounded-4"/>
+            </div>
+            <div className="row mt-2 text-dark">
+              <div className="col-3">
+                <FontAwesomeIcon icon={faComment} />
+                <span>{post.reply}</span>
+              </div>
+              <div className="col-3">
+                <FontAwesomeIcon icon={faRetweet} />
+                <span>{post.retweet}</span>
+              </div>
+              <div className="col-3">
+                <FontAwesomeIcon icon={faHeart} />
+                <span>{post.like}</span>
+              </div>
+              <div className="col-3">
+                <FontAwesomeIcon icon={faArrowUp} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+  )
+}
+export default PostItem;
---
+++ src/tuiter/navigation-sidebar/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {useLocation} from "react-router";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {
+  faHome,
+  faHashtag,
+  faBell,
+  faFileAlt,
+  faEnvelope,
+  faBookmark,
+  faList,
+  faUser,
+  faEllipsisH
+} from '@fortawesome/free-solid-svg-icons'
+
+const NavigationSidebar = () => {
+  const {pathname} = useLocation();
+  const paths = pathname.split('/')
+  const active = paths[2];
+  return (
+      <div className="list-group">
+        <a className="list-group-item">Tuiter</a>
+        <Link to="/tuiter/home"
+              className={`list-group-item d-flex align-items-center 
+              ${active === 'home' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faHome}/>
+          <span className="d-none d-xl-block ms-2">Home</span>
+        </Link>
+        <Link to="/tuiter/explore"
+              className={`list-group-item d-flex align-items-center 
+              ${active === 'explore' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faHashtag}/>
+          <span className="d-none d-xl-block ms-2">Explore</span>
+        </Link>
+        <Link to="/" className="list-group-item d-flex align-items-center">
+          <FontAwesomeIcon icon={faFileAlt}/>
+          <span className="d-none d-xl-block ms-2">Labs</span>
+        </Link>
+        <a className={`list-group-item d-flex align-items-center
+           ${active === 'notifications' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faBell}/>
+          <span className="d-none d-xl-block ms-2">Notifications</span>
+        </a>
+        <a className={`list-group-item d-flex align-items-center
+           ${active === 'messages' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faEnvelope}/>
+          <span className="d-none d-xl-block ms-2">Messages</span>
+        </a>
+        <a className={`list-group-item d-flex align-items-center
+           ${active === 'bookmarks' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faBookmark}/>
+          <span className="d-none d-xl-block ms-2">Bookmarks</span>
+        </a>
+        <a className={`list-group-item d-flex align-items-center
+           ${active === 'lists' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faList}/>
+          <span className="d-none d-xl-block ms-2">Lists</span>
+        </a>
+        <a className={`list-group-item d-flex align-items-center
+           ${active === 'profile' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faUser}/>
+          <span className="d-none d-xl-block ms-2">Profile</span>
+        </a>
+        <a className={`list-group-item d-flex align-items-center
+           ${active === 'more' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faEllipsisH}/>
+          <span className="d-none d-xl-block ms-2">More</span>
+        </a>
+      </div>
+  );
+};
+export default NavigationSidebar;
