89d7c10cc189e1fd2226c482333ba3a3854cf6da
rxjiang <jiang.rux@northeastern.edu>
2023-03-30 16:30:39 -0700

    a8 REST fa22

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -18,65 +18,69 @@ const TuitStats = ({ tuit }) => {
   const dispatch = useDispatch();
   return (
     <div className="row mt-2 text-dark">
-      <div className="col-2">
-        <FontAwesomeIcon icon={faComment} className="me-2" />
-        <span>{tuit.replies}</span>
-      </div>
-      <div className="col-2">
-        <FontAwesomeIcon icon={faRetweet} className="me-2" />
-        <span>{tuit.retuits}</span>
-      </div>
-      <div className="col-2">
-        <FontAwesomeIcon
-          icon={tuit.liked ? faHeart : farHeart}
-          className={["me-2", tuit.liked ? "text-danger" : ""].join(" ")}
-          onClick={() => {
-            if (tuit.liked) {
-              dispatch(
-                updateTuitThunk({
-                  ...tuit,
-                  likes: tuit.likes - 1,
-                  liked: false,
-                })
-              );
-            } else {
-              dispatch(
-                updateTuitThunk({
-                  ...tuit,
-                  likes: tuit.likes + 1,
-                  liked: true,
-                })
-              );
-            }
-          }}
-        />
-        <span>{tuit.likes}</span>
-      </div>
-      <div className="col-2">
-        <FontAwesomeIcon
-          icon={tuit.disliked ? faThumbsDown : farThumbsDown}
-          className={"me-2"}
-          onClick={() => {
-            if (tuit.disliked) {
-              dispatch(
-                updateTuitThunk({
-                  ...tuit,
-                  dislikes: tuit.dislikes - 1,
-                  disliked: false,
-                })
-              );
-            } else {
-              dispatch(
-                updateTuitThunk({
-                  ...tuit,
-                  dislikes: tuit.dislikes + 1,
-                  disliked: true,
-                })
-              );
-            }
-          }}
-        />
-        <span>{tuit.dislikes}</span>
+      <div className="col-10">
+        <div className="row">
+          <div className="col-3">
+            <FontAwesomeIcon icon={faComment} className="me-2" />
+            <span>{tuit.replies}</span>
+          </div>
+          <div className="col-3">
+            <FontAwesomeIcon icon={faRetweet} className="me-2" />
+            <span>{tuit.retuits}</span>
+          </div>
+          <div className="col-3">
+            <FontAwesomeIcon
+              icon={tuit.liked ? faHeart : farHeart}
+              className={["me-2", tuit.liked ? "text-danger" : ""].join(" ")}
+              onClick={() => {
+                if (tuit.liked) {
+                  dispatch(
+                    updateTuitThunk({
+                      ...tuit,
+                      likes: tuit.likes - 1,
+                      liked: false,
+                    })
+                  );
+                } else {
+                  dispatch(
+                    updateTuitThunk({
+                      ...tuit,
+                      likes: tuit.likes + 1,
+                      liked: true,
+                    })
+                  );
+                }
+              }}
+            />
+            <span>{tuit.likes}</span>
+          </div>
+          <div className="col-3">
+            <FontAwesomeIcon
+              icon={tuit.disliked ? faThumbsDown : farThumbsDown}
+              className={"me-2"}
+              onClick={() => {
+                if (tuit.disliked) {
+                  dispatch(
+                    updateTuitThunk({
+                      ...tuit,
+                      dislikes: tuit.dislikes - 1,
+                      disliked: false,
+                    })
+                  );
+                } else {
+                  dispatch(
+                    updateTuitThunk({
+                      ...tuit,
+                      dislikes: tuit.dislikes + 1,
+                      disliked: true,
+                    })
+                  );
+                }
+              }}
+            />
+            <span>{tuit.dislikes}</span>
+          </div>
+        </div>
       </div>
       <div className="col-2">
         <FontAwesomeIcon icon={faShare} className="me-2" />
